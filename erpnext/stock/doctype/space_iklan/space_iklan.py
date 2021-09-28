fae23f2b7d4e26f434447d1f350fa21fb17b673b
berkat28 <bersamakreatifteknologi.otp@gmail.com>
2021-09-28 15:40:32 +0700

    new update from PC2

---
+++ erpnext/stock/doctype/space_iklan/space_iklan.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Ming Promotion and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+class SpaceIklan(WebsiteGenerator):
+	pass
