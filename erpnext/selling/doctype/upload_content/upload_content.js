3f4a20e5967faf6b20b6886dbac6d211f5aa2cb2
berkat28 <bersamakreatifteknologi.otp@gmail.com>
2021-10-15 15:46:06 +0700

    new update from PC2--Julia

--- erpnext/selling/doctype/upload_content/upload_content.js
+++ erpnext/selling/doctype/upload_content/upload_content.js
@@ -2,7 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Upload Content', {
-	// refresh: function(frm) {
-
+	// refresh:  function(frm){
+	// 	if (frm.doc.user == "" || frm.doc.user == null) {
+	// 		frm.doc.user = frappe.session.user;
+	// 	}
+	// }
 	// }
 });
