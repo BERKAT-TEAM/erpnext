abb4ef21a46d64c5dff55e73086cdfaffdc7f3d7
skhotijah <skhotijah0902@gmail.com>
2021-10-24 11:31:59 +0700

    update and change from siti fixing conflict

--- erpnext/selling/doctype/job_order/job_order.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Ming Promotion and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Job Order', {
-	// refresh: function(frm) {
-
-	// }
-});
